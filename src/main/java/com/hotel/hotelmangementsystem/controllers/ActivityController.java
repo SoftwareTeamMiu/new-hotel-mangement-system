6455e04898ac20b1397764761c6d2c97736f6595
Ahmedehab280202 <ahmedehab280202@gmail.com>
2023-04-28 22:15:56 +0300

    Create Activity Controller & Service

---
+++ src/main/java/com/hotel/hotelmangementsystem/controllers/ActivityController.java
@@ -0,0 +1,42 @@
+package com.hotel.hotelmangementsystem.controllers;
+
+import com.hotel.hotelmangementsystem.models.Activity;
+import com.hotel.hotelmangementsystem.services.ActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/activities")
+public class ActivityController {
+    @Autowired
+    private ActivityService activityService;
+    @GetMapping("/")
+    public List<Activity> getAllActivities() {
+        return activityService.getAllActivities();
+    }
+    @GetMapping("/{id}")
+    public Activity getActivityById(@PathVariable Integer id) {
+        return activityService.getActivityById(id);
+    }
+    @PostMapping("/")
+    public Activity createActivity(@RequestBody Activity activity) {
+        return activityService.createOrUpdateActivity(new Activity(activity.getDurationHrs(), activity.getDate(), activity.getHostName()));
+    }
+    @PutMapping("/{id}")
+    public Activity updateTodo(@PathVariable Integer id, @RequestBody Activity activity) {
+        Activity existingActivity = activityService.getActivityById(id);
+        if (existingActivity == null) {
+            return null;
+        }
+        existingActivity.setDurationHrs(activity.getDurationHrs());
+        existingActivity.setDate(activity.getDate());
+        existingActivity.setHostName(activity.getHostName());
+        return activityService.createOrUpdateActivity(existingActivity);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteTodoById(@PathVariable Integer id) {
+        activityService.deleteActivityById(id);
+    }
+}
