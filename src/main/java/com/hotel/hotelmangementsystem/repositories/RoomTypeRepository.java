917c6439e444551d9f5bfa7a39f7c66acf6558e3
ali ismail <aliismail106@gmail.com>
2023-05-02 04:11:43 +0300

    RoomType implemented

---
+++ src/main/java/com/hotel/hotelmangementsystem/repositories/RoomTypeRepository.java
@@ -0,0 +1,6 @@
+package com.hotel.hotelmangementsystem.repositories;
+import com.hotel.hotelmangementsystem.models.RoomType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomTypeRepository extends JpaRepository<RoomType, Integer> {
+}
